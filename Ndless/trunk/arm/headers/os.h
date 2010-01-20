d8d4aeea0e43557878d0d30e7dcb1ef937b81f09
Geoffrey Anneheim <geoffrey.anneheim@free.fr>
2010-01-20 18:10:35 +0000

    / Fixed the file 'headers/os.h'. + Added a script to assemble user programs.
    git-svn-id: https://www.unsads.com/scm/svn/nsptools@4

--- Ndless/trunk/arm/headers/os.h
+++ Ndless/trunk/arm/headers/os.h
@@ -1,7 +1,7 @@
 #ifdef CAS
-  .include "headers/os_cas_1.1.9170.h"
+  #include "os_cas_1.1.9170.h"
 #elif NON_CAS
-  .include "headers/os_1.1.9253.h"
+  #include "os_1.1.9253.h"
 #else
   #error You must specify a Nspire hardware (CAS or NON-CAS)
 #endif
---
+++ Ndless/trunk/arm/user/assemble.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Cygwin required to run this file: http://www.cygwin.com/
+if [ $# -ne 2 ]; then
+	echo "Usage: assemble.sh <file.s> <nspire_hardware>"
+	exit 0
+fi
+infile="$1"
+infile_noext="${infile%.S}"
+nspire_hardware="$2"
+if [ ! -e "$infile" ]; then
+	echo "'$infile' not found"
+	exit 1
+fi
+
+arm-elf-gcc -mcpu=arm7tdmi -E "$infile_noext".S -o "tmp_""$infile_noext".S -D ${nspire_hardware}
+arm-elf-as -mcpu=arm7tdmi -o "$infile_noext".o "tmp_""$infile_noext".S
+arm-elf-ld -o "$infile_noext".elf "$infile_noext".o
+arm-elf-objcopy -O binary "$infile_noext".elf "$infile_noext".bin
+rm -f "tmp_""$infile_noext".S "$infile_noext".o "$infile_noext".elf
+cp -f "$infile_noext".bin ../../res/${nspire_hardware}/userfiles/"$infile_noext".tns
