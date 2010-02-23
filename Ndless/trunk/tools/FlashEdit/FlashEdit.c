0e1ac60fd2ec84f5b712417d752014a3a941c3ac
Geoffrey Anneheim <geoffrey.anneheim@free.fr>
2010-02-23 21:30:24 +0000

    / Find the offset of the resource '/phoenix/syst/locales/en/strings.res' if the argument is omitted.
    git-svn-id: https://www.unsads.com/scm/svn/nsptools@55

--- Ndless/trunk/tools/FlashEdit/FlashEdit.c
+++ Ndless/trunk/tools/FlashEdit/FlashEdit.c
@@ -8,10 +8,17 @@
 #define FLASH_PAGE_BYTES_SIZE           512
 #define HEADER_ORIG_RESOURCE            0x6801
 
+#define RESOURCE_CONTENT_BEGIN_BYTES_SIZE 16
+
+static const unsigned char s_resourceContentBegin[2][16] = {
+  {0x68, 0x01, 0x00, 0x00, 0x0E, 0x26, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00, 0x45, 0x6E, 0x67, 0x6C}, // NON CAS
+  {0x68, 0x01, 0x00, 0x00, 0x12, 0x26, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00, 0x45, 0x6E, 0x67, 0x6C}, // CAS
+};
+
 void printUsage() {
   puts(
-    "Usage: FlashEdit -b <strbackup.tns> <flash_rom.bin> <resource_offset>\n"
-    "       FlashEdit <loader.tns> <flash_rom.bin> <resource_offset>\n"
+    "Usage: FlashEdit -b <strbackup.tns> <flash_rom.bin> [<resource_offset>]\n"
+    "       FlashEdit <loader.tns> <flash_rom.bin> [<resource_offset>]\n"
     "       \n"
     "   -b                backup the original strings.res\n"
     "   <strbackup.tns>   backup file to create\n"
@@ -37,6 +44,22 @@ int getFileSize(FILE *f) {
   fseek (f, pos, SEEK_SET);
 
   return end;
+}
+
+unsigned long findResourceOffset(FILE* fFlash) {
+  static unsigned char buffer[sizeof(s_resourceContentBegin)];
+  
+  fseek(fFlash, 0, SEEK_SET);
+  while (fread(buffer, 1, RESOURCE_CONTENT_BEGIN_BYTES_SIZE, fFlash) == RESOURCE_CONTENT_BEGIN_BYTES_SIZE) {
+    if ((memcmp(buffer, s_resourceContentBegin[0], RESOURCE_CONTENT_BEGIN_BYTES_SIZE) == 0)
+     || (memcmp(buffer, s_resourceContentBegin[1], RESOURCE_CONTENT_BEGIN_BYTES_SIZE) == 0)
+    )
+    {
+      return (ftell(fFlash) - RESOURCE_CONTENT_BEGIN_BYTES_SIZE);
+    }
+  }
+  
+  return 0;
 }
 
 void backup(FILE* fOutResource, FILE* fFlash, unsigned long resource_offset) {
@@ -76,61 +99,89 @@ int main(int argc, char* argv[]) {
   char *pathNewResource = NULL, *pathFlash = NULL, *pathOrigResource = NULL, *strOffset = NULL;
   int fileNewResourceSize;
   char headerOrigResource[2];
-  unsigned long resource_offset;
+  unsigned long resource_offset = 0;
+  int bFindResourceOffset = 0;
   
-  if (argc == 5 && !strcmp(argv[1], "-b")) {
+  if ((argc == 4 || argc == 5) && !strcmp(argv[1], "-b")) {
     pathOrigResource = argv[2];
     pathFlash = argv[3];
-    strOffset = argv[4];
-  } else if (argc == 4) {
+    
+    if (argc == 4) {
+      bFindResourceOffset = -1;
+    }
+    else {
+      strOffset = argv[4];
+    }
+  } else if (argc == 3 || argc == 4) {
     pathNewResource = argv[1];
     pathFlash = argv[2];
-    strOffset = argv[3];
+    if (argc == 3) {
+      bFindResourceOffset = -1;
+    }
+    else {
+      strOffset = argv[3];
+    }
   } else {
     printUsage();
     return -1; 
   }
   
-  resource_offset = strtoul(strOffset, 0, 16);
   fileFlash = fopen(pathFlash, "r+b");
   if (fileFlash == NULL) {
-    fclose(fileNewResource);
-    printErrorAndExit("Could not open <flash_rom.bin>.", 2);  
+    printErrorAndExit("Could not open <flash_rom.bin>.", -2);  
+  }
+  
+  if (bFindResourceOffset) {
+    resource_offset = findResourceOffset(fileFlash);
+    if (resource_offset == 0) {
+      printErrorAndExit("Could not find the resource offset in <flash_rom.bin>.", -5);
+    }
+    printf("Resource offset located at 0x%08lx\n", resource_offset);
+  }
+  else {
+    if (strOffset[1] == 'x' || strOffset[1] == 'X') {
+      resource_offset = strtoul(strOffset, 0, 16);
+    }
+    else {
+      resource_offset = strtoul(strOffset, 0, 10);
+    }
   }
   
   // install
   if (pathNewResource) {
     fileNewResource = fopen(pathNewResource, "rb");
     if (fileNewResource == NULL) {
-      printErrorAndExit("Could not open <infile.tns>.", 1); 
+      fclose(fileFlash);
+      printErrorAndExit("Could not open <infile.tns>.", -1); 
     }
     
     fileNewResourceSize = getFileSize(fileNewResource);
     if (fileNewResourceSize != LOADER_RES_FILE_BYTES_SIZE) {
       printf("Error: <infile.tns> size must be of %d bytes\n", LOADER_RES_FILE_BYTES_SIZE);
       fclose(fileNewResource);
-      exit(3);
+      exit(-3);
     }
     
     edit(fileNewResource, fileFlash, resource_offset);
     fclose(fileNewResource);
     printf("File '%s' successfully modified!\n", pathFlash);
-
   }
   
   // backup
   if (pathOrigResource) {
     fileOrigResource = fopen(pathOrigResource, "w+b");
-      if (fileOrigResource == NULL) {
-        printErrorAndExit("Could not create <strbackup.tns>.", 3);  
+    if (fileOrigResource == NULL) {
+      fclose(fileFlash);
+      printErrorAndExit("Could not create <strbackup.tns>.", -3);  
     }
+    
     backup(fileOrigResource, fileFlash, resource_offset);
     fseek (fileOrigResource, 0, SEEK_SET);
     if (fread(headerOrigResource, 1, sizeof(headerOrigResource), fileOrigResource) !=  sizeof(headerOrigResource)
       || (headerOrigResource[0] << 8) + headerOrigResource[1] != HEADER_ORIG_RESOURCE) {
         fclose(fileOrigResource);
         unlink(pathOrigResource);
-        printErrorAndExit("Resource not found in ROM image (the image must be an original one)", 4);
+        printErrorAndExit("Resource not found in ROM image (the image must be an original one)", -4);
     }
     fclose(fileOrigResource);
     printf("File '%s' successfully created!\n", pathOrigResource);
@@ -138,5 +189,5 @@ int main(int argc, char* argv[]) {
   
   fclose(fileFlash);
   
-  return EXIT_SUCCESS;  
+  return resource_offset;  
 }
--- Ndless/trunk/tools/FlashEdit/install-ndless-nspire_emu.bat
+++ Ndless/trunk/tools/FlashEdit/install-ndless-nspire_emu.bat
@@ -5,26 +5,20 @@ REM CAS or NON_CAS
 set HARDWARE=NON_CAS
 REM ##########################
 
-if "%HARDWARE%" == "CAS" (
-  set OFFSET=0x5DC990
-  echo "here"
-) else (
-  set OFFSET=0x613830
-)
 echo Creating strbackup.tns...
-FlashEdit -b strbackup.tns "%NSPIRE_EMU_FLASH_IMAGE_PATH%" %OFFSET%
-if not %errorlevel% == 0 goto end
+FlashEdit -b ../../res/%HARDWARE%/strbackup.tns "%NSPIRE_EMU_FLASH_IMAGE_PATH%"
+if %errorlevel% LEQ 0 goto end
 echo.
 echo Now:
 echo - run the flash image with nspire_emu
 echo - set the system language to English
 echo - create a directory called 'ndless'
-echo - send to this directory the files 'strbackup.tns' and 'res/%HARDWARE%/boot.tns'
+echo - send to this directory the files 'res/%HARDWARE%/strbackup.tns', 'res/%HARDWARE%/loader.tns' and 'res/%HARDWARE%/hook.tns'
 echo - save the flash image
 echo Then press enter.
 pause
 echo Installing the loader...
-FlashEdit ../../res/%HARDWARE%/loader.tns "%NSPIRE_EMU_FLASH_IMAGE_PATH%" %OFFSET%
+FlashEdit ../../res/%HARDWARE%/loader.tns "%NSPIRE_EMU_FLASH_IMAGE_PATH%" %errorlevel%
 echo.
 echo Ndless is now installed in the flash image. You can restart nspire_emu.
 :end
