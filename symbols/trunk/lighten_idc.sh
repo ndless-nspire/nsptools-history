356e9884c9a652e3cdff0d2d39518510aeeb10a8
Olivier Armand <olivier.calc@gmail.com>
2010-08-13 18:14:07 +0000

    - Add OS_ncas-1.7.idc - lighten_idc.sh: more specific filter for strings
    git-svn-id: https://www.unsads.com/scm/svn/nsptools@154

--- symbols/trunk/lighten_idc.sh
+++ symbols/trunk/lighten_idc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Usage: lighten_idc.sh <in >out
 # Keeps what should be shared from an IDA .idc file
-egrep 'MakeName|MakeRptCmt' | egrep -v '"a' | egrep -v 'null|jumptable|default'
+egrep 'MakeName|MakeRptCmt' | egrep -v '"a[A-Z]' | egrep -v 'null|jumptable|default'
