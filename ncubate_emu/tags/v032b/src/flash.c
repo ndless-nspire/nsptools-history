8aa751239c60ebd0140efcad54049cb647cb0c65
Olivier Armand <olivier.calc@gmail.com>
2010-11-06 15:41:22 +0000

    tag v032b
    git-svn-id: https://www.unsads.com/scm/svn/nsptools@302

---
+++ ncubate_emu/tags/v032b/src/flash.c
@@ -0,0 +1,281 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "emu.h"
+
+struct flash {
+	u32 operation;
+	union {
+		u32 full_address;
+		struct {
+			u8 column_address;
+			u8 page_address_lo;
+			u8 page_address_mid;
+			u8 page_address_hi;
+		};
+	};
+	u32 op_size;
+	u32 ram_address;
+	u16 area;             /* 0x000=A, 0x100=B, 0x200=C */
+} flash;
+
+/* 64k pages of 512+16 bytes each (32 MB total) */
+#define NUM_BLOCKS 0x800
+#define PAGES_PER_BLOCK 0x20
+#define PAGE_SIZE 0x210
+
+#define NUM_PAGES (NUM_BLOCKS * PAGES_PER_BLOCK)
+#define BLOCK_SIZE (PAGES_PER_BLOCK * PAGE_SIZE)
+
+u8 flash_data[NUM_PAGES][PAGE_SIZE];
+u8 flash_block_modified[NUM_BLOCKS];
+
+static void *ram_ptr_nonull(u32 addr, u32 size) {
+	void *p = phys_mem_ptr(addr, size);
+	if (!p)
+		error("NAND flash: address %x is not in RAM\n", addr);
+	return p;
+}
+
+static void flash_read_page() {
+	u32 page = flash.full_address >> 8;
+	if (page >= NUM_PAGES)
+		error("NAND flash: read nonexistent page %x", page);
+
+	if (flash.ram_address != 0) {
+		u8 *pagedata = flash_data[page];
+		u32 offset = flash.area + flash.column_address;
+		if (offset + flash.op_size > PAGE_SIZE)
+			error("NAND flash: read too long (%d bytes from %x)", flash.op_size, offset);
+
+		memcpy(ram_ptr_nonull(flash.ram_address, flash.op_size), &pagedata[offset], flash.op_size);
+	}
+}
+
+static void flash_erase_block() {
+	u32 page = flash.full_address & 0xffffff;
+	if (page & (-NUM_PAGES | (PAGES_PER_BLOCK - 1)))
+		error("NAND flash: erase nonexistent block %x", page);
+	memset(&flash_data[page][0], 0xFF, BLOCK_SIZE);
+	flash_block_modified[page / PAGES_PER_BLOCK] = true;
+}
+
+static void flash_program_page() {
+	u32 page = flash.full_address >> 8;
+	if (page >= NUM_PAGES)
+		error("NAND flash: program nonexistent page %x", page);
+
+	u32 offset = /*flash.area + */flash.column_address;
+	u32 i;
+	if (offset + flash.op_size > PAGE_SIZE)
+		error("NAND flash: program too long (%d bytes from %x)", flash.op_size, offset);
+
+	u8 *pagedata = &flash_data[page][offset];
+	u8 *progdata = ram_ptr_nonull(flash.ram_address + offset, flash.op_size);
+	for (i = 0; i < flash.op_size; i++)
+		pagedata[i] &= progdata[i];
+	flash_block_modified[page / PAGES_PER_BLOCK] = true;
+}
+
+static void flash_begin_op() {
+	logprintf(LOG_FLASH, "NAND flash: op=%06x addr=%08x size=%08x raddr=%08x\n",
+		flash.operation, flash.full_address, flash.op_size, flash.ram_address);
+
+	switch (flash.operation & 0xFF) {
+		case 0x00: /* Read Area A */
+			flash.area = 0;
+			flash_read_page();
+			break;
+		case 0x50: /* Read Area C */
+			flash.area = 0x200;
+			flash_read_page();
+			break;
+		case 0x60: /* Block Erase */
+			flash_erase_block();
+			break;
+		case 0x70: /* Read Status Register */
+			break;
+		case 0x80: /* Page Program */
+			flash_program_page();
+			break;
+		case 0x90: /* Read Electronic Signature */
+			/* ST Micro NAND256R3A */
+			memcpy(ram_ptr_nonull(flash.ram_address, 2), "\x20\x35", 2);
+			break;
+		case 0xFF: /* Reset */
+			break;
+		default:
+			error("NAND flash: Unknown operation type");
+	}
+}
+
+u32 nand_flash_read_word(u32 addr) {
+	switch (addr & 0x3FFFFFF) {
+		case 0x00: return 0; /* ??? */
+		case 0x08: return 0; /* "Operation in progress" register */
+		case 0x34: return 0x40; /* Status register (bit 0 = error, bit 6 = ready, bit 7 = writeprot */
+		case 0x40: return 1; /* ??? */
+		case 0x44: return 0xFFFFFF; /* Calculate page ECC */
+	}
+	return bad_read_word(addr);
+}
+
+void nand_flash_write_word(u32 addr, u32 value) {
+	switch (addr & 0x3FFFFFF) {
+		case 0x00: return;
+		case 0x04: return;
+		case 0x08:
+			if (value != 1)
+				error("NAND flash: wrote something other than 1 to reg 8");
+			flash_begin_op();
+			return;
+		case 0x0C: flash.operation = value;        return;
+		case 0x10: flash.column_address = value;   return;
+		case 0x14: flash.page_address_lo = value;  return;
+		case 0x18: flash.page_address_mid = value; return;
+		case 0x1C: flash.page_address_hi = value;  return;
+		case 0x20: return; /* ??? */
+		case 0x24: flash.op_size = value;          return;
+		case 0x28: flash.ram_address = value;      return;
+		case 0x2C: return; /* AHB speed / 2500000 */
+		case 0x30: return; /* ASB speed / 250000  */
+		case 0x40: return;
+		case 0x44: return;
+		case 0x48: return;
+		case 0x4C: return;
+		case 0x50: return;
+		case 0x54: return;
+	}
+	bad_write_word(addr, value);
+}
+
+char flash_filename[MAX_PATH];
+
+void flash_save_changes() {
+	FILE *f = fopen(flash_filename, "r+b");
+	if (!f) {
+		printf("NAND flash: could not open ");
+		perror(flash_filename);
+		exit(1);
+	}
+	u32 block, count = 0;
+	for (block = 0; block < NUM_BLOCKS; block++) {
+		if (flash_block_modified[block]) {
+			fseek(f, block * BLOCK_SIZE, SEEK_SET);
+			fwrite(flash_data[block * PAGES_PER_BLOCK], BLOCK_SIZE, 1, f);
+			flash_block_modified[block] = false;
+			count++;
+		}
+	}
+	fclose(f);
+	printf("NAND flash: saved %d modified blocks to file\n", count);
+}
+
+int flash_save_as(void) {
+	char abs_filename[MAX_PATH];
+	realpath(flash_filename, abs_filename);
+	strcpy(flash_filename, abs_filename);
+	printf("Saving flash image %s...\n", flash_filename);
+	FILE *f = fopen(flash_filename, "wb");
+	if (!f) {
+		printf("NAND flash: could not open ");
+		perror(flash_filename);
+		exit(1);
+	}
+	if (!fwrite(flash_data, sizeof flash_data, 1, f) || fflush(f)) {
+		fclose(f);
+		remove(flash_filename);
+		printf("NAND flash: could not write to ");
+		perror(flash_filename);
+		exit(1);
+	}
+	fclose(f);
+	memset(flash_block_modified, 0, sizeof(flash_block_modified));
+	printf("Flash image saved.\n");
+	return 0;
+}
+
+static int preload(int page, char *name, char *filename) {
+	FILE *f = fopen(filename, "rb");
+	if (!f) {
+		perror(filename);
+		exit(1);
+	}
+
+	fseek(f, 0, SEEK_END);
+	u32 size = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	int offset = 0x20;
+
+	strcpy((char *)&flash_data[page][0], name);
+	*(u32 *)&flash_data[page][20] = BSWAP32(0x55F00155);
+	*(u32 *)&flash_data[page][24] = 0;
+	*(u32 *)&flash_data[page][28] = BSWAP32(size);
+
+	while (fread(&flash_data[page][offset], 1, 0x200 - offset, f)) {
+		page++;
+		if (page == NUM_PAGES) {
+			printf("Preload image(s) too large\n");
+			exit(1);
+		}
+		offset = 0;
+	}
+
+	// Round to next block
+	return (page + 0x1F) & -0x20;
+}
+
+void flash_load(const char *filename) {
+	realpath(filename, flash_filename);
+	FILE *f = fopen(filename, "r+b");
+	if (!f) {
+		perror(filename);
+		exit(1);
+	}
+	if (!fread(flash_data, sizeof flash_data, 1, f)) {
+		printf("Could not read flash image from %s\n", flash_filename);
+		exit(1);
+	}
+	fclose(f);
+	memset(flash_block_modified, 0, sizeof(flash_block_modified));
+}
+
+void flash_reload(void) {
+	flash_load(flash_filename);
+}
+
+void flash_initialize(char *preload_boot2, char *preload_diags, char *preload_os) {
+	memset(flash_data, 0xFF, sizeof flash_data);
+
+	/* After creating the filesystem, the first 16kB block of flash is
+	 * copied to a file called /phoenix/manuf.dat. To pass OS validation,
+	 * that file must contain the following values. */
+	*(u32 *)&flash_data[0][0] = 0x796EB03C;
+	/* First two hex digits of product ID */
+	*(u16 *)&flash_data[4][4] = emulate_cas ? 0x0C : 0x0E;
+	/* Third hex digit of product ID. Exact value doesn't seem to matter,
+	 * but must be < 0x10. */
+	*(u16 *)&flash_data[4][6] = 0;
+
+	int page = 0x1000;
+	if (preload_boot2) page = preload(page, "***PRELOAD_BOOT2***", preload_boot2);
+	if (preload_diags) page = preload(page, "***PRELOAD_DIAGS***", preload_diags);
+	if (preload_os)    page = preload(page, "***PRELOAD_IMAGE***", preload_os);
+}
+
+struct flash_saved_state {
+	struct flash flash;
+};
+
+void *flash_save_state(size_t *size) {
+	*size = sizeof(struct flash_saved_state);
+	struct flash_saved_state *state = malloc(*size);
+	memcpy(&state->flash, &flash, sizeof(flash));
+	return state;
+}
+
+void flash_reload_state(void *state) {
+	struct flash_saved_state *_state = (struct flash_saved_state *)state;
+	memcpy(&flash, &_state->flash, sizeof(flash));
+}
---
+++ ncubate_emu/tags/v032b/src/memory.c
@@ -0,0 +1,439 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "emu.h"
+
+u8   (*read_byte_map[64])(u32 addr);
+u16  (*read_half_map[64])(u32 addr);
+u32  (*read_word_map[64])(u32 addr);
+void (*write_byte_map[64])(u32 addr, u8 value);
+void (*write_half_map[64])(u32 addr, u16 value);
+void (*write_word_map[64])(u32 addr, u32 value);
+
+/* For invalid/unknown physical addresses */
+u8 bad_read_byte(u32 addr)               { warn("Bad read_byte: %08x", addr); debugger(DBG_EXCEPTION, 0); return 0; }
+u16 bad_read_half(u32 addr)              { warn("Bad read_half: %08x", addr); debugger(DBG_EXCEPTION, 0); return 0; }
+u32 bad_read_word(u32 addr)              { warn("Bad read_word: %08x", addr); debugger(DBG_EXCEPTION, 0); return 0; }
+void bad_write_byte(u32 addr, u8 value)  { warn("Bad write_byte: %08x %02x",addr,value); debugger(DBG_EXCEPTION, 0); }
+void bad_write_half(u32 addr, u16 value) { warn("Bad write_half: %08x %04x",addr,value); debugger(DBG_EXCEPTION, 0); }
+void bad_write_word(u32 addr, u32 value) { warn("Bad write_word: %08x %08x",addr,value); debugger(DBG_EXCEPTION, 0); }
+
+u8 mem_and_flags[MEM_SIZE * 2];
+const struct mem_area_desc mem_areas[] = {
+	{ 0x00000000, ROM_SIZE,    rom_00 },
+	{ 0x10000000, RAM_10_SIZE, ram_10 },
+	{ 0xA4000000, RAM_A4_SIZE, ram_A4 },
+};
+
+void *phys_mem_ptr(u32 addr, u32 size) {
+	int i;
+	for (i = 0; i < 3; i++) {
+		u32 offset = addr - mem_areas[i].base;
+		if (offset < mem_areas[i].size && size <= mem_areas[i].size - offset)
+			return mem_areas[i].ptr + offset;
+	}
+	return NULL;
+}
+
+void *virt_mem_ptr(u32 addr, u32 size) {
+	return phys_mem_ptr(mmu_translate(addr, NULL), size);
+}
+
+#define DO_READ_ACTION (RF_READ_BREAKPOINT)
+void read_action(u32 addr) {
+	if (!gdb_connected)
+		printf("Hit read breakpoint at %08x. Entering debugger.\n", addr);
+	debugger(DBG_READ_BREAKPOINT, addr);
+}
+
+#define DO_WRITE_ACTION (RF_WRITE_BREAKPOINT | RF_CODE_TRANSLATED | RF_CODE_NO_TRANSLATE)
+void write_action(u32 *flags, u32 addr) {
+	if (*flags & RF_WRITE_BREAKPOINT) {
+		if (!gdb_connected)
+			printf("Hit write breakpoint at %08x. Entering debugger.\n", addr);
+		debugger(DBG_WRITE_BREAKPOINT, addr);
+	}
+	if (*flags & RF_CODE_TRANSLATED) {
+		logprintf(LOG_CPU, "Wrote to translated code at %08x. Deleting translations.\n", addr);
+		invalidate_translation(*flags >> RFS_TRANSLATION_INDEX);
+	} else {
+		*flags &= ~RF_CODE_NO_TRANSLATE;
+	}
+}
+
+/* 00000000, 10000000, A4000000: ROM and RAM */
+u8 memory_read_byte(u32 addr) {
+	u8 *ptr = phys_mem_ptr(addr, 1);
+	if (!ptr) return bad_read_byte(addr);
+	if (RAM_FLAGS((size_t)ptr & ~3) & DO_READ_ACTION) read_action(addr);
+	return *ptr;
+}
+u16 memory_read_half(u32 addr) {
+	u16 *ptr = phys_mem_ptr(addr, 2);
+	if (!ptr) return bad_read_half(addr);
+	if (RAM_FLAGS((size_t)ptr & ~3) & DO_READ_ACTION) read_action(addr);
+	return *ptr;
+}
+u32 memory_read_word(u32 addr) {
+	u32 *ptr = phys_mem_ptr(addr, 4);
+	if (!ptr) return bad_read_word(addr);
+	if (RAM_FLAGS(ptr) & DO_READ_ACTION) read_action(addr);
+	return *ptr;
+}
+void memory_write_byte(u32 addr, u8 value) {
+	u8 *ptr = phys_mem_ptr(addr, 1);
+	if (!ptr) { bad_write_byte(addr, value); return; }
+	u32 *flags = &RAM_FLAGS((size_t)ptr & ~3);
+	if (*flags & RF_READ_ONLY) { bad_write_byte(addr, value); return; }
+	if (*flags & DO_WRITE_ACTION) write_action(flags, addr);
+	*ptr = value;
+}
+void memory_write_half(u32 addr, u16 value) {
+	u16 *ptr = phys_mem_ptr(addr, 2);
+	if (!ptr) { bad_write_half(addr, value); return; }
+	u32 *flags = &RAM_FLAGS((size_t)ptr & ~3);
+	if (*flags & RF_READ_ONLY) { bad_write_half(addr, value); return; }
+	if (*flags & DO_WRITE_ACTION) write_action(flags, addr);
+	*ptr = value;
+}
+void memory_write_word(u32 addr, u32 value) {
+	u32 *ptr = phys_mem_ptr(addr, 4);
+	if (!ptr) { bad_write_word(addr, value); return; }
+	u32 *flags = &RAM_FLAGS(ptr);
+	if (*flags & RF_READ_ONLY) { bad_write_word(addr, value); return; }
+	if (*flags & DO_WRITE_ACTION) write_action(flags, addr);
+	*ptr = value;
+}
+
+/* 8FFF0000 */
+void unknown_8C_write_word(u32 addr, u32 value) {
+	switch (addr - 0x8FFF0000 ) {
+		case 0x00: return;
+		case 0x04: return;
+		case 0x08: return;
+		case 0x0C: return;
+		case 0x10: return;
+		case 0x14: return;
+	}
+	bad_write_word(addr, value);
+}
+
+/* A9000000: SPI - newly used in OS 2.1 */
+u32 spi_read_word(u32 addr) {
+	switch (addr - 0xA9000000) {
+		case 0x10: return -1; // returning 0 hangs
+		case 0x1C: return 0;
+		case 0x20: return 0;
+	}
+	return bad_read_word(addr);
+}
+void spi_write_word(u32 addr, u32 value) {
+	switch (addr - 0xA9000000) {
+		case 0x0C: return;
+		case 0x1C: return;
+		case 0x20: return;
+	}
+	bad_write_word(addr, value);
+}
+
+/* AC000000: SDIO */
+u8 sdio_read_byte(u32 addr) {
+	switch (addr & 0x3FFFFFF) {
+		case 0x29: return -1;
+	}
+	return bad_read_byte(addr);
+}
+u16 sdio_read_half(u32 addr) {
+	switch (addr & 0x3FFFFFF) {
+		case 0x10: return -1;
+		case 0x12: return -1;
+		case 0x14: return -1;
+		case 0x16: return -1;
+		case 0x18: return -1;
+		case 0x1A: return -1;
+		case 0x1C: return -1;
+		case 0x1E: return -1;
+		case 0x2C: return -1;
+		case 0x30: return -1;
+	}
+	return bad_read_half(addr);
+}
+u32 sdio_read_word(u32 addr) {
+	switch (addr & 0x3FFFFFF) {
+		case 0x20: return -1;
+	}
+	return bad_read_word(addr);
+}
+void sdio_write_byte(u32 addr, u8 value) {
+	switch (addr & 0x3FFFFFF) {
+		case 0x29: return;
+		case 0x2E: return;
+		case 0x2F: return;
+	}
+	bad_write_byte(addr, value);
+}
+void sdio_write_half(u32 addr, u16 value) {
+	switch (addr & 0x3FFFFFF) {
+		case 0x04: return;
+		case 0x0C: return;
+		case 0x0E: return;
+		case 0x2C: return;
+		case 0x30: return;
+		case 0x32: return;
+		case 0x34: return;
+		case 0x36: return;
+	}
+	bad_write_half(addr, value);
+}
+void sdio_write_word(u32 addr, u32 value) {
+	switch (addr & 0x3FFFFFF) {
+		case 0x00: return;
+		case 0x08: return;
+		case 0x20: return;
+	}
+	bad_write_word(addr, value);
+}
+
+/* B0000000 (and B4000000?): USB */
+u8 usb_read_byte(u32 addr) {
+	if ((addr & 0x1FF) == 0x100) return 0x40; // operational registers start at +40
+	return bad_read_byte(addr);
+}
+u16 usb_read_half(u32 addr) {
+	if ((addr & 0x1FF) == 0x102) return 0x0100; // EHCI 1.0
+	return bad_read_half(addr);
+}
+u32 usb_read_word(u32 addr) {
+	switch (addr & 0x1FF) {
+		case 0x00C: return 0;
+		case 0x100: return 0x01000040; // EHCI 1.0, operational registers start at +40
+		case 0x104: return 0x00010011; // Port indicator control, port power control, 1 port
+		case 0x108: return 6;
+		case 0x10C: return 0;
+		case 0x124: return 0;
+		case 0x140: return 0;
+		case 0x148: return 0;
+		case 0x164: return 0;
+		case 0x180: return 0;
+		case 0x184: return 0;
+		case 0x188: return 0;
+		// returning 0 causes OS 1.1 to detect a "probe" (?)
+		// and hang trying to communicate with it
+		case 0x1A4: return -1;
+		case 0x1B0: return 0;
+		case 0x1B4: return 0;
+		case 0x1C0: return 0;
+	}
+	return bad_read_word(addr);
+}
+void usb_write_word(u32 addr, u32 value) {
+	switch (addr & 0x1FF) {
+		case 0x080: return; // used by diags
+		case 0x084: return; // used by diags
+		case 0x140: return;
+		case 0x144: return;
+		case 0x148: return;
+		case 0x150: return;
+		case 0x158: return;
+		case 0x180: return;
+		case 0x184: return;
+		case 0x188: return;
+		case 0x1A4: return;
+		case 0x1A8: return;
+		case 0x1AC: return;
+		case 0x1B4: return;
+		case 0x1C0: return;
+	}
+	bad_write_word(addr, value);
+}
+
+/* BC000000 */
+u32 unknown_BC_read_word(u32 addr) {
+	switch (addr & 0x3FFFFFF) {
+		case 0xC: return 0;
+	}
+	return bad_read_word(addr);
+}
+
+/* C4000000: ADC (Analog-to-Digital Converter) */
+static u32 adc_int_active;
+static u32 adc_int_enable;
+
+static u32 adc_read_word(u32 addr) {
+	if (!(addr & 0x100)) {
+		switch (addr & 0x3FFFFFF) {
+			case 0x00: case 0x04:
+				return adc_int_active;
+			case 0x08:
+				return adc_int_enable;
+		}
+	} else {
+		int channel = addr >> 5 & 7;
+		switch (addr & 0x3FFFE1F) {
+			case 0x00:
+				return 0;
+			case 0x10:
+				// Scale for channels 1-2:   155 units = 1 volt
+				// Scale for other channels: 310 units = 1 volt
+				if (channel == 3) {
+					// A value from 0 to 20 indicates normal TI-Nspire keypad.
+					// A value from 21 to 42 indicates TI-84+ keypad.
+					return 10 + (keypad_type * 21);
+				} else {
+					return 930;
+				}
+		}
+	}
+	return bad_read_word(addr);
+}
+static void adc_write_word(u32 addr, u32 value) {
+	if (!(addr & 0x100)) {
+		switch (addr & 0x3FFFFFF) {
+			case 0x04: // Acknowledge interrupt?
+				adc_int_active &= ~value;
+				if (adc_int_active == 0)
+					int_set(INT_ADC, 0);
+				return;
+			case 0x08: // Interrupt enable?
+				adc_int_enable = value;
+				return;
+			case 0x10: return;
+			case 0x14: return;
+		}
+	} else {
+		int n = addr >> 5 & 7;
+		switch (addr & 0x3FFFE1F) {
+			case 0x00:
+				if (value & 1) {
+					// start a measurement, I guess
+					adc_int_active |= 2 << (n << 2);
+					int_set(INT_ADC, 1);
+				}
+				return;
+			case 0x04:
+			case 0x08:
+			case 0x0C:
+			case 0x14:
+				return;
+		}
+	}
+	return bad_write_word(addr, value);
+}
+
+/* -------------------------------------------------------------------------- */
+
+/* If read=false, then its a write. Returns the physical address. */
+static inline u32 check_action(u32 vaddr, bool read, u8 size) {
+	u32 paddr = mmu_translate(vaddr, data_abort);
+	u8 *ptr = phys_mem_ptr(paddr, size);
+	if (ptr) {
+		u32 *flags = &RAM_FLAGS((size_t)ptr & ~3);
+		if (read && *flags & DO_READ_ACTION)  read_action(vaddr);
+		else if    (*flags & DO_WRITE_ACTION) write_action(flags, vaddr);
+	}
+	return paddr;
+}
+
+u32 __attribute__((fastcall)) slow_read_byte(u32 addr) {
+	return read_byte_map[addr >> 26](check_action(addr, true, 1));
+}
+
+u32 __attribute__((fastcall)) slow_read_half(u32 addr) {
+	if (addr & 1) error("Unaligned read_half: %x", addr);
+	return read_half_map[addr >> 26](check_action(addr, true, 2));
+}
+
+u32 __attribute__((fastcall)) slow_read_word(u32 addr) {
+	if (addr & 3) error("Unaligned read_word: %x", addr);
+	return read_word_map[addr >> 26](check_action(addr, true, 4));
+}
+
+void __attribute__((fastcall)) slow_write_byte(u32 addr, u32 value) {
+	write_byte_map[addr >> 26](check_action(addr, false, 1), value);
+}
+
+void __attribute__((fastcall)) slow_write_half(u32 addr, u32 value) {
+	if (addr & 1) error("Unaligned write_half: %x", addr);
+	write_half_map[addr >> 26](check_action(addr, false, 2), value);
+}
+
+void __attribute__((fastcall)) slow_write_word(u32 addr, u32 value) {
+	if (addr & 3) error("Unaligned write_word: %x", addr);
+	write_word_map[addr >> 26](check_action(addr, false, 4), value);
+}
+
+void memory_initialize() {
+	int i;
+	for (i = 0; i < 64; i++) {
+		/* will fallback to bad_* on non-memory addresses */
+		read_byte_map[i] = memory_read_byte;
+		read_half_map[i] = memory_read_half;
+		read_word_map[i] = memory_read_word;
+		write_byte_map[i] = memory_write_byte;
+		write_half_map[i] = memory_write_half;
+		write_word_map[i] = memory_write_word;
+	}
+
+	write_word_map[0x8F >> 2] = unknown_8C_write_word;
+
+	read_byte_map[0x90 >> 2] = apb_read_byte;
+	read_half_map[0x90 >> 2] = apb_read_half;
+	read_word_map[0x90 >> 2] = apb_read_word;
+	write_byte_map[0x90 >> 2] = apb_write_byte;
+	write_half_map[0x90 >> 2] = apb_write_half;
+	write_word_map[0x90 >> 2] = apb_write_word;
+
+	read_word_map[0xA9 >> 2] = spi_read_word;
+	write_word_map[0xA9 >> 2] = spi_write_word;
+
+	read_byte_map[0xAC >> 2] = sdio_read_byte;
+	read_half_map[0xAC >> 2] = sdio_read_half;
+	read_word_map[0xAC >> 2] = sdio_read_word;
+	write_byte_map[0xAC >> 2] = sdio_write_byte;
+	write_half_map[0xAC >> 2] = sdio_write_half;
+	write_word_map[0xAC >> 2] = sdio_write_word;
+
+	read_byte_map[0xB0 >> 2] = usb_read_byte;
+	read_half_map[0xB0 >> 2] = usb_read_half;
+	read_word_map[0xB0 >> 2] = usb_read_word;
+	write_word_map[0xB0 >> 2] = usb_write_word;
+
+	read_word_map[0xB8 >> 2] = nand_flash_read_word;
+	write_word_map[0xB8 >> 2] = nand_flash_write_word;
+
+	read_word_map[0xBC >> 2] = unknown_BC_read_word;
+
+	read_word_map[0xC0 >> 2] = lcd_read_word;
+	write_word_map[0xC0 >> 2] = lcd_write_word;
+
+	read_word_map[0xC4 >> 2] = adc_read_word;
+	write_word_map[0xC4 >> 2] = adc_write_word;
+
+	read_word_map[0xCC >> 2] = sha256_read_word;
+	write_word_map[0xCC >> 2] = sha256_write_word;
+
+	read_word_map[0xDC >> 2] = int_read_word;
+	write_word_map[0xDC >> 2] = int_write_word;
+}
+
+struct memory_saved_state {
+	u8 mem_and_flags[MEM_SIZE * 2];
+	u32 adc_int_active;
+	u32 adc_int_enable;
+};
+
+void *memory_save_state(size_t *size) {
+	*size = sizeof(struct memory_saved_state);
+	struct memory_saved_state *state = malloc(*size);
+	memcpy(&state->mem_and_flags, mem_and_flags, sizeof(mem_and_flags));
+	state->adc_int_active = adc_int_active;
+	state->adc_int_enable = adc_int_enable;
+	return state;
+}
+
+void memory_reload_state(void *state) {
+	struct memory_saved_state *_state = (struct memory_saved_state *)state;
+	memcpy(mem_and_flags, &_state->mem_and_flags, sizeof(mem_and_flags));
+	adc_int_active = _state->adc_int_active;
+	adc_int_enable = _state->adc_int_enable;
+}
