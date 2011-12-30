8ad2ded7b1958b532bab4a7f6ab32c16ee9f1af1
Olivier Armand <olivier.calc@gmail.com>
2011-12-30 15:11:44 +0000

    idb2idc.sh: only v3.1
    git-svn-id: https://www.unsads.com/scm/svn/nsptools@486

--- symbols/trunk/idb2idc.sh
+++ symbols/trunk/idb2idc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Convert all the files in the current folder
 [ -n "$1" -a -f "$1" ] && idbs="$1" || idbs="`ls *.idb`"
-#idbs=OS_ncas-3.1.0.idb
+idbs="OS_ncas-3.1.0.idb OS_cas-3.1.0.idb OS_ncascx-3.1.0.idb OS_cascx-3.1.0.idb"
 for idb in $idbs; do
 	idc=${idb%%.idb}.idc
 	echo "$idc..."
