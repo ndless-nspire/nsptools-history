60f81590bab5f6a1d4f5ef5ae159a1b50ba67db6
Olivier Armand <olivier.calc@gmail.com>
2012-04-14 10:26:21 +0000

    merge Levak's
    git-svn-id: https://www.unsads.com/scm/svn/nsptools@606

--- symbols/trunk/addheader.sh
+++ symbols/trunk/addheader.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ $# -ne 1 ]; then
+	echo "Usage: addheader.sh <raw.idc>"
+	echo "Writes it to out.idc"
+	exit 1
+fi
 echo "#include <idc.idc>" > out.idc
 echo "static main(void) {" >> out.idc
 cat $1 >> out.idc
