9cc75386b9e0a5cb42dc807b1ad57797a3b5a02b
Olivier Armand <olivier.calc@gmail.com>
2012-11-03 18:05:42 +0000

    now uses Ndless's USB definitions
    git-svn-id: https://www.unsads.com/scm/svn/nsptools@712

--- usb/trunk/usb.c
+++ usb/trunk/usb.c
@@ -1,52 +1,8 @@
 #include <os.h>
-
-static const unsigned device_get_ivars_addrs[] = {0x10354C20, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define device_get_ivars SYSCALL_CUSTOM(device_get_ivars_addrs, void *, device_t dev)
-
-static const unsigned device_get_softc_addrs[] = {0x10354C60, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define device_get_softc SYSCALL_CUSTOM(device_get_softc_addrs, void *, device_t dev)
-
 #include <ocd.h>
-#include "usbdi.h"
-#include "usb.h"
+#include <usbdi.h>
+#include <usb.h>
 #include <nspireio2.h>
-
-static const unsigned register_driver_addrs[] = {0x10354E44, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define register_driver SYSCALL_CUSTOM(register_driver_addrs, int, int a, int (*methods[])(device_t), const char *driver_name, int b, unsigned softc_size)
-
-static const unsigned init_usb_stack_addrs[] = {0x1035F334, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define init_usb_stack SYSCALL_CUSTOM(init_usb_stack_addrs, void)
-
-static const unsigned unregister_drivers_addrs[] = {0x10352F88, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define unregister_drivers SYSCALL_CUSTOM(unregister_drivers_addrs, void)
-
-static const unsigned register_drivers_addrs[] = {0x10352F88, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define register_drivers SYSCALL_CUSTOM(register_drivers_addrs, void)
-
-static const unsigned usbd_do_request_addrs[] = {0x103F4270, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define usbd_do_request SYSCALL_CUSTOM(usbd_do_request_addrs, usbd_status, usbd_device_handle dev, usb_device_request_t * req, void *data)
-
-static const unsigned usbd_get_device_descriptor_addrs[] = {0x103F32E8, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define usbd_get_device_descriptor SYSCALL_CUSTOM(usbd_get_device_descriptor_addrs, usb_device_descriptor_t *, usbd_device_handle dev)
-
-static const unsigned usbd_devinfo_addrs[] = {0x103F2E58, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define usbd_devinfo SYSCALL_CUSTOM(usbd_devinfo_addrs, void, usbd_device_handle dev, int showclass, char *cp)
-
-static const unsigned usbd_interface2endpoint_descriptor_addrs[] = {0x103F32F0, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define usbd_interface2endpoint_descriptor SYSCALL_CUSTOM(usbd_interface2endpoint_descriptor_addrs, usb_endpoint_descriptor_t *, usbd_interface_handle iface, u_int8_t index)
-
-static const unsigned usbd_get_interface_descriptor_addrs[] = {0x103F32E0, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define usbd_get_interface_descriptor SYSCALL_CUSTOM(usbd_get_interface_descriptor_addrs, usb_interface_descriptor_t *, usbd_interface_handle iface)
-
-static const unsigned usbd_interface2device_handle_addrs[] = {0x103F3388, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define usbd_interface2device_handle SYSCALL_CUSTOM(usbd_interface2device_handle_addrs, void, usbd_interface_handle iface, usbd_device_handle *dev)
-
-static const unsigned usbd_open_pipe_intr_addrs[] = {0x103F4548, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define usbd_open_pipe_intr SYSCALL_CUSTOM(usbd_open_pipe_intr_addrs, usbd_status, usbd_interface_handle iface, u_int8_t address, u_int8_t flags, usbd_pipe_handle *pipe, usbd_private_handle priv, void *buffer, u_int32_t len, usbd_callback cb, int ival)
-
-// TODO replace with malloc(), should work
-static const unsigned bsd_malloc_addrs[] = {0x1035F998, 0, 0, 0}; // non-CAS 3.1, CAS 3.1, non-CAS CX 3.1, CAS CX 3.1 addresses
-#define bsd_malloc SYSCALL_CUSTOM(bsd_malloc_addrs, void*, size_t size, BOOL init)
 
 /* usbhid.h */ 
 
@@ -72,6 +28,7 @@ static void init_console(void) {
 /** TODO
 - free du malloc dans softc
 - crash si shutdown. Manque methode non implémentée ?
+- manque unregister. Fonction non trouvée dans OS, existe ?
 */
 
 static int match(device_t self) {
@@ -201,7 +158,7 @@ char HIDtoa(struct s_boot_kbd_report *buf, unsigned index) {
 	return( 0x07 );         //Bell 
 }
 
-static void ukbd_intr(usbd_xfer_handle xfer, usbd_private_handle addr, usbd_status status) {
+static void ukbd_intr(usbd_xfer_handle __attribute__((unused)) xfer, usbd_private_handle addr, usbd_status  __attribute__((unused)) status) {
 	struct ukbd_softc *sc = addr;
 	struct s_boot_kbd_report *ibuf = (struct s_boot_kbd_report *)sc->sc_ibuf.buf;
 	unsigned i;
@@ -214,7 +171,7 @@ static void ukbd_intr(usbd_xfer_handle xfer, usbd_private_handle addr, usbd_stat
 static int ums_xcoord = 0;
 static int ums_ycoord = 0;
 
-static void ums_intr(usbd_xfer_handle xfer, usbd_private_handle addr, usbd_status status) {
+static void ums_intr(usbd_xfer_handle __attribute__((unused)) xfer, usbd_private_handle addr, usbd_status __attribute__((unused)) status) {
 	struct ukbd_softc *sc = addr;
 	struct s_boot_ums_report *ibuf = (struct s_boot_ums_report *)sc->sc_ibuf.buf;
 	ums_xcoord += (int)(ibuf->xdispl / 2);
@@ -241,7 +198,7 @@ static int attach(device_t self) {
 	sc->sc_ep_addr = ed->bEndpointAddress;
 	//sc->sc_isize = 8;
 	sc->sc_isize = 16;
-	sc->sc_ibuf.buf = bsd_malloc(sc->sc_isize, TRUE);
+	sc->sc_ibuf.buf = malloc(sc->sc_isize);
 	if (!sc->sc_ibuf.buf)
 		return (ENXIO);
 	usbd_set_protocol(sc->sc_iface, 0);
@@ -257,7 +214,7 @@ static int attach(device_t self) {
 	return 0;
 }
 
-static int detach(device_t self) {
+static int detach(device_t __attribute__((unused)) self) {
 	//putStr(0, 0, "                                                  ", 15, 0);
 	return 0;
 }
@@ -266,7 +223,7 @@ static int (*methods[])(device_t) = {match, attach, detach, NULL};
 
 int register_my_driver(void) {
 	// softc_size: must not be 0
-	return register_driver(2, methods, "mydriver", 0, sizeof(struct ukbd_softc));
+	return usb_register_driver(2, methods, "mydriver", 0, sizeof(struct ukbd_softc));
 }
 
 int main(void) {
