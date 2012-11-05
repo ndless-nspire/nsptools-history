add2acb4eaa2fc11b2540ea9cc591ecd7445f3cf
Olivier Armand <olivier.calc@gmail.com>
2012-11-05 21:08:52 +0000

    ukbd: key mapping with trtabs
    git-svn-id: https://www.unsads.com/scm/svn/nsptools@723

--- usb/trunk/ukbd.c
+++ usb/trunk/ukbd.c
@@ -52,6 +52,80 @@ struct s_boot_kbd_report {
 	char keycode[6];
 };
 
+// USB usage ID to TI-Nspire scancode as expected by get_event() / send_key_event()
+// See http://www.usb.org/developers/devclass_docs/Hut1_11.pdf chap. 10.
+static char trtab_key[256] = {
+	0x00, 0x00, 0x00, 0x00, 0x66, 0x46, 0x26, 0x85, /* 00 - 07 */
+	0x65, 0x45, 0x25, 0x84, 0x64, 0x44, 0x24, 0x83, /* 08 - 0F */
+	0x63, 0x43, 0x23, 0x82, 0x62, 0x42, 0x22, 0x81, /* 10 - 17 */
+	0x61, 0x41, 0x21, 0x80, 0x60, 0x40, 0x00, 0x00, /* 18 - 1F */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 20 - 27 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 28 - 2F */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 30 - 37 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 38 - 3F */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 40 - 47 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 48 - 4F */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 50 - 57 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 58 - 5F */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 60 - 67 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 68 - 6F */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 70 - 77 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 78 - 7F */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 80 - 87 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 88 - 8F */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 90 - 97 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 98 - 9F */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* A0 - A7 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* A8 - AF */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* B0 - B7 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* B8 - BF */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* C0 - C7 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* C8 - CF */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* D0 - D7 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* D8 - DF */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* E0 - E7 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* E8 - EF */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* F0 - F7 */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* F8 - FF */   
+};                                                
+
+// USB usage ID to ASCII char as expected by get_event() / send_key_event()
+// See http://www.usb.org/developers/devclass_docs/Hut1_11.pdf chap. 10.
+static char trtab_ascii[256] = {
+	'\0', '\0', '\0', '\0', 'a' , 'b' , 'c' , 'd' , /* 00 - 07 */
+	'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , /* 08 - 0F */
+	'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , /* 10 - 17 */
+	'u' , 'v' , 'w' , 'x' , 'y' , 'z' , '\0', '\0', /* 18 - 1F */
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 20 - 27 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 28 - 2F */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 30 - 37 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 38 - 3F */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 40 - 47 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 48 - 4F */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 50 - 57 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 58 - 5F */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 60 - 67 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 68 - 6F */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 70 - 77 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 78 - 7F */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 80 - 87 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 88 - 8F */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 90 - 97 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* 98 - 9F */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* A0 - A7 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* A8 - AF */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* B0 - B7 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* B8 - BF */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* C0 - C7 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* C8 - CF */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* D0 - D7 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* D8 - DF */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* E0 - E7 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* E8 - EF */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* F0 - F7 */  
+	'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', /* F8 - FF */
+};
+
 // From https://github.com/felis/lightweight-usb-host/blob/e790c8c7e29f04fd938939f4dd5f88b52125b8af/cli.c
 // TODO cleanup
 /* function to convert HID keyboard code to ASCII */
@@ -105,13 +179,8 @@ char HIDtoa(struct s_boot_kbd_report *buf, unsigned index) {
 	return( 0x07 );         //Bell 
 }
 
-// from 'A' to 'Z'
-unsigned char ascii_to_ns_key[] = {0x66, 0x46, 0x26, 0x85, 0x65, 0x45, 0x25, 0x84, 0x64, 0x44 /* J */, 0x24, 0x83, 0x63, 0x43, 0x23, 0x82, 0x62, 0x42, 0x22 /* S */, 0x81, 0x61, 0x41, 0x21, 0x80, 0x60, 0x40};
-
-unsigned short ascii_to_ns_key_ascii(char ascii) {
-	if (!ascii || ascii > 'z' || ascii < 'a')
-		return 0;
-	return ascii_to_ns_key[ascii - 'a'] << 8 | ascii;
+unsigned short usage_id_to_ns_key_ascii(unsigned char usage_id) {
+	return trtab_key[usage_id] << 8 | trtab_ascii[usage_id];
 }
 
 static void ukbd_intr(usbd_xfer_handle __attribute__((unused)) xfer, usbd_private_handle addr, usbd_status  __attribute__((unused)) status) {
@@ -119,9 +188,9 @@ static void ukbd_intr(usbd_xfer_handle __attribute__((unused)) xfer, usbd_privat
 	struct s_boot_kbd_report *ibuf = (struct s_boot_kbd_report *)sc->sc_ibuf.buf;
 	unsigned i;
 		struct s_ns_event ns_ev;
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < 6 /* sizeof((struct s_boot_kbd_report).keycode) */; i++) {
 		if (ibuf->keycode[i]) {
-			unsigned short ns_key_ascii = ascii_to_ns_key_ascii(HIDtoa(ibuf, i));
+			unsigned short ns_key_ascii = usage_id_to_ns_key_ascii(ibuf->keycode[i]);
 			send_key_event(&ns_ev, ns_key_ascii, FALSE, TRUE);
 			send_key_event(&ns_ev, ns_key_ascii, TRUE, TRUE);
 		}
