33ac8fb1d85f7a3bee1f0a73ab61b7e6f03f745d
Olivier Armand <olivier.calc@gmail.com>
2013-12-23 10:46:22 +0000

    more CAS CX 3.6 addresses
    git-svn-id: https://www.unsads.com/scm/svn/nsptools@934

--- symbols/trunk/idb2idc.sh
+++ symbols/trunk/idb2idc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Convert all the files in the current folder
-[ -n "$1" -a -f "$1" ] && idbs="$1" || idbs="OS_ncas-3.1.0.idb OS_cas-3.1.0.idb OS_ncascx-3.1.0.idb OS_cascx-3.1.0.idb OS_cmc-3.1.0.idb OS_cascmc-3.1.0.idb"
+[ -n "$1" -a -f "$1" ] && idbs="$1" || idbs="OS_cascx-3.6.0.idb"
 for idb in $idbs; do
 	idc=${idb%%.idb}.idc
 	echo "$idc..."
